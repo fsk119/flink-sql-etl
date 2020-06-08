d7d9832a57809790c14f608095d09889ad1389d5
fsk119 <1059623455@qq.com>
2020-06-08 11:22:23 +0800

    avro test

--- etl-job/src/main/resources/job-sql-1.11/kafka2kafka/KafkaAvro2Kafka.sql
+++ etl-job/src/main/resources/job-sql-1.11/kafka2kafka/KafkaAvro2Kafka.sql
@@ -1,18 +1,21 @@
--- first job: build avro format data from csv and write to kafka topic
-create table csv( 
-    user_name VARCHAR, is_new BOOLEAN, content VARCHAR
-) with ( 
-    'connector' = 'filesystem',
-    'path' = '/Users/ohmeatball/Work/flink-sql-etl/data-generator/src/main/resources/user.csv',
-    'format' = 'csv')
---BUG: it will throw NPE: FLINK-1940
+-- first job: build avro format data from csv and write to kafka nqtopic
+CREATE TBALE csv( 
+  user_name VARCHAR, 
+  is_new BOOLEAN, 
+  content VARCHAR
+) WITH ( 
+  'connector' = 'filesystem',
+  'path' = '/Users/ohmeatball/Work/flink-sql-etl/data-generator/src/main/resources/user.csv',
+  'format' = 'csv'
+)
+
 CREATE TABLE AvroTest (
   user_name VARCHAR,
   is_new BOOLEAN,
-  content VARCHAR) WITH (
+  content VARCHAR
+) WITH (
   'connector' = 'kafka',
   'topic' = 'avro_from_csv',
-  'properties.zookeeper.connect' = 'localhost:2181',
   'properties.bootstrap.servers' = 'localhost:9092',
   'properties.group.id' = 'testGroup3',
   'scan.startup.mode' = 'earliest-offset',
@@ -25,10 +28,10 @@ insert into AvroTest select user_name, is_new, content from csv
 CREATE TABLE AvroTest (
   user_name VARCHAR,
   is_new BOOLEAN,
-  content VARCHAR) WITH (
+  content VARCHAR
+) WITH (
   'connector' = 'kafka',
   'topic' = 'avro_from_csv',
-  'properties.zookeeper.connect' = 'localhost:2181',
   'properties.bootstrap.servers' = 'localhost:9092',
   'properties.group.id' = 'testGroup4',
   'scan.startup.mode' = 'earliest-offset',
@@ -38,14 +41,15 @@ CREATE TABLE AvroTest (
 CREATE TABLE WikipediaFeed_filtered (
   user_name STRING,
   is_new    BOOLEAN,
-  content STRING) WITH (
+  content STRING
+) WITH (
   'connector' = 'kafka',
   'topic' = 'WikipediaFeed2_filtered',
-  'properties.zookeeper.connect' = 'localhost:2181',
   'properties.bootstrap.servers' = 'localhost:9092',
   'properties.group.id' = 'testGroup3',
   'scan.startup.mode' = 'earliest-offset',
-  'format' = 'avro')
-insert into WikipediaFeed_filtered
-select user_name, is_new, content
-from AvroTest
+  'format' = 'avro'
+)
+INSERT INTO  WikipediaFeed_filtered
+SELECT user_name, is_new, content
+FROM AvroTest
